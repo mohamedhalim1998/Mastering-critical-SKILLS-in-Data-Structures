3d3a578df1b16bf24cc7bb9f1843d0759af530b2
root <root@DESKTOP-NKAOJDL.localdomain>
2022-05-20 14:59:15 +0200

    implement SLL Homework 2

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,21 @@
 public class Main {
     public static void main(String[] args) {
         SinglyLinkedList list = new SinglyLinkedList();
+        list.insert(1);
+        list.insert(2);
+        list.insert(3);
         list.insert(4);
         list.insert(5);
         list.insert(6);
-        list.insertFront(3);
-        list.insertFront(2);
-        list.insertFront(1);
+//        list.insertSorted(10);
+//        list.insertSorted(2);
+//        list.insertSorted(30);
+//        list.insertSorted(4);
+//        list.insertSorted(1);
         System.out.println(list);
-        list.delete(2);
+        list.reverseInPlace();
         System.out.println(list);
+
 
 
 
