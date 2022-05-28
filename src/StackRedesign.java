4075bc1f41d32a439c78db3b3d67d1c945687b00
MohamedHalim <mohamedhalim1998@gmail.com>
2022-05-28 21:14:12 +0200

    implement Stack Homework 1

---
+++ src/StackRedesign.java
@@ -0,0 +1,62 @@
+import java.util.Arrays;
+
+public class StackRedesign {
+    int[] arr;
+    int addedElements;
+    int size;
+
+    public StackRedesign(int size) {
+        this.size = size;
+        this.arr = new int[size];
+    }
+
+    public boolean push(int value) {
+        if (isFull()) {
+            return false;
+        }
+        for (int i = addedElements - 1; i >= 0; i--) {
+            arr[i + 1] = arr[i];
+        }
+        arr[0] = value;
+        addedElements++;
+        return true;
+    }
+
+    public int pop() {
+        if (isEmpty()) {
+            throw new RuntimeException("stack is full");
+        }
+        int value = arr[0];
+        for (int i = 0; i < addedElements - 1; i++) {
+            arr[i] = arr[i + 1];
+        }
+        arr[addedElements - 1] = 0;
+        addedElements--;
+        return value;
+    }
+
+    public int peek() {
+        if (isEmpty()) {
+            throw new RuntimeException("stack is full");
+        }
+        return arr[0];
+    }
+
+
+    public boolean isEmpty() {
+        return addedElements == 0;
+    }
+
+    public boolean isFull() {
+        return addedElements == size;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(arr);
+    }
+
+    public void display() {
+        System.out.println(this);
+    }
+}
