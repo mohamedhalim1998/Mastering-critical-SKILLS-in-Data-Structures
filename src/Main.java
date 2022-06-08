640b69b99d2b8f0550abc9367f1093a11bffb32b
MohamedHalim <mohamedhalim1998@gmail.com>
2022-06-08 20:26:33 +0200

    implement SLL Homework 1

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,31 @@
 public class Main {
     public static void main(String[] args) {
-        Vector vector = new Vector();
-        vector.add(1);
-        vector.add(2);
-        vector.add(3);
-        vector.add(4);
-        vector.add(5);
-        vector.add(6);
-        vector.add(7);
-        vector.insert(2, 8);
-        System.out.println(vector.find(1));
-        System.out.println(vector.find(10));
-        System.out.println(vector.get(5));
-        System.out.println(vector.getFront());
-        System.out.println(vector.getBack());
-        System.out.println(vector);
-        vector.rotateRight();
-        System.out.println(vector);
-        vector.rotateLeft();
-        System.out.println(vector);
-        System.out.println(vector.remove(2));
-        System.out.println(vector);
-        vector.rotateRight(5);
-        System.out.println(vector);
+        SinglyLinkedList list = new SinglyLinkedList();
+        list.insert(4);
+        list.insert(5);
+        list.insert(6);
+        list.insertFront(3);
+        list.insertFront(2);
+        list.insertFront(1);
+        System.out.println(list);
+        list.deleteFront();
+        System.out.println(list);
+        System.out.println(list.get(0));
+        System.out.println(list.get(1));
+        System.out.println(list.get(2));
+        System.out.println(list.get(3));
+        System.out.println(list.get(4));
+        System.out.println(list.getFromBack(0));
+        System.out.println(list.getFromBack(1));
+
+        SinglyLinkedList list2 = new SinglyLinkedList();
+        list2.insert(2);
+        list2.insert(3);
+        list2.insert(4);
+        list2.insert(5);
+        list2.insert(6);
+        System.out.println(list);
+        System.out.println(list2);
+        System.out.println(list.equals(list2));
     }
 }
---
+++ src/SinglyLinkedList.java
@@ -0,0 +1,162 @@
+import java.util.Objects;
+
+public class SinglyLinkedList {
+    private Node head;
+    private Node tail;
+    int size;
+
+    public void insert(int x) {
+        Node node = new Node(x);
+        if (head == null) {
+            head = node;
+            tail = head;
+        } else {
+            tail.next = node;
+            tail = node;
+        }
+        size++;
+    }
+
+    public void insertFront(int x) {
+        Node node = new Node(x);
+        if (head == null) {
+            head = node;
+            tail = head;
+        } else {
+            node.next = head;
+            head = node;
+        }
+        size++;
+    }
+
+    public int deleteFront() {
+        if (size == 0) {
+            throw new RuntimeException("delete from an empty list");
+        }
+        int item = head.x;
+        head = head.next;
+        size--;
+        return item;
+    }
+
+
+    public int get(int index) {
+        if (index >= size) {
+            return -1;
+        }
+        Node node = head;
+        int i = 0;
+        while (i < index && node.next != null) {
+            i++;
+            node = node.next;
+        }
+        if (i == index) {
+            return node.x;
+        }
+        return -1;
+    }
+
+    public int getFromBack(int index) {
+        return get(size - index - 1);
+    }
+
+    public int search(int item) {
+        if (size == 0) {
+            return -1;
+        }
+        Node node = head;
+        int index = 0;
+        while (node != null && node.x != item) {
+            node = node.next;
+            index++;
+        }
+        if (node != null)
+            return index;
+        return -1;
+    }
+
+    public int improvedSearch(int item) {
+        if (size == 0) {
+            return -1;
+        }
+        Node node = head;
+        int index = 0;
+        Node prev = null;
+        while (node != null && node.x != item) {
+            prev = node;
+            node = node.next;
+            index++;
+        }
+        if (node != null) {
+            if (prev != null) {
+                int temp = node.x;
+                node.x = prev.x;
+                prev.x = temp;
+            }
+            return index;
+        }
+        return -1;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder out = new StringBuilder();
+        out.append("SingleLinkedList{");
+        Node node = head;
+        if (node != null) {
+            while (node.next != null) {
+                out.append(node.x).append(", ");
+                node = node.next;
+            }
+            out.append(node.x).append("}");
+        }
+        return out.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        SinglyLinkedList that = (SinglyLinkedList) o;
+        if (size != that.size)
+            return false;
+        Node n1 = this.head;
+        Node n2 = that.head;
+        while (n1 != null && n2 != null) {
+            if (!n1.equals(n2)) {
+                return false;
+            }
+            n1 = n1.next;
+            n2 = n2.next;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(head, tail, size);
+    }
+
+    private class Node {
+        private int x;
+        private Node next;
+
+        public Node(int x) {
+            this.x = x;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Node node = (Node) o;
+            return x == node.x;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(x);
+        }
+    }
+
+}
